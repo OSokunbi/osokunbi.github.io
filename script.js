0f55b318fbcd4f17393846fd76834ad6ed43c66e
Oreoluwa Sokunbi <osokunbi@umich.edu>
2024-06-16 21:17:51 -0400

    clean

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
 function setTheme() {
   // localStorage.clear();
-  console.log('settheme called')
   var theme = localStorage.getItem('theme');
-  console.log('theme gotten')
-  console.log(theme)
 
 
   if (theme) {
-    console.log('yes theme')
     if (theme == "dark-theme") {
       document.getElementById('body').classList.add('dark-theme');
-      console.log('existing, set to dark-theme')
       document.getElementById('theme-icon').setAttribute('name','sunny-outline');
     }
 
     if (theme == "light-theme") {
       document.getElementById('body').classList.add('light-theme');
-      console.log('existing, set to light-theme')
       document.getElementById('theme-icon').setAttribute('name','moon-outline');
     } 
   } else {
-    console.log('no theme')
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
       document.getElementById('body').classList.add('dark-theme');
-      console.log('new, set to dark-theme')
       document.getElementById('theme-icon').setAttribute('name','sunny-outline');
     } else {
       document.getElementById('body').classList.add('light-theme');
       localStorage.setItem('theme', 'light-theme');
-      console.log('new, set to light-theme')
       document.getElementById('theme-icon').setAttribute('name','moon-outline');
     }
   }
